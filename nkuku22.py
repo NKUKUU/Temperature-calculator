3fbdd75e3d0535382538903539214b6d9afc471b
NKUKUU <106319704+NKUKUU@users.noreply.github.com>
2022-06-21 15:46:19 +0300

    Update nkuku22.py

--- nkuku22.py
+++ nkuku22.py
@@ -11,3 +11,12 @@ fahrenheit= st.number_input("enter tempereture")
 celcius=(fahrenheit-32)*5/9
 st.write("% 2f fahrenheit is: % 0.2f celsius" % (fahrenheit,celcius))
 
+username="nkuku"
+password="124"
+x=st.text_input("enter username :")
+y=st.number_input("enter password:")
+if x==username and y==password:
+    st,write("yes")
+else:
+    st.write("no")
+    
